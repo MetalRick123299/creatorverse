349dc4a669a47a147f74e2e0acb88c3fe94929d6
Richard Rodriguez <richardbr.personal@gmail.com>
2024-08-10 13:43:05 -0400

    All Creators & Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,15 @@
-function Card() {
-  return <div>Card</div>;
+import { Creator } from "../utils/types";
+
+function Card({ creator }: { creator: Creator }) {
+  const { name, description, imageURL, url } = creator;
+  return (
+    <div>
+      <h3>{name}</h3>
+      <img src={imageURL} alt={name} />
+      <p>{description}</p>
+      <a href={url}>Link</a>
+    </div>
+  );
 }
 
 export default Card;
--- src/pages/ShowCreators.tsx
+++ src/pages/ShowCreators.tsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../utils/client";
+import Card from "../components/Card";
+import { Creator } from "../utils/types";
+
 function ShowCreators() {
-  return <div>ShowCreators</div>;
+  const [creators, setCreators] = useState<Creator[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const creators = await supabase.from("creators").select("*");
+
+      if (!creators.data) return;
+      if (creators.error) {
+        console.error("Error");
+        return;
+      }
+
+      setCreators(creators.data);
+    })();
+  }, []);
+
+  return (
+    <div>
+      <h1>ShowCreators</h1>
+      {creators.length > 0 ? (
+        creators.map((creator) => <Card creator={creator} />)
+      ) : (
+        <p>No creators found</p>
+      )}
+    </div>
+  );
 }
 
 export default ShowCreators;
