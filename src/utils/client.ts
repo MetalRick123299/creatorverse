9cd86a94637a6a6ffe93b16f1b3f6bef255d3d0a
Richard Rodriguez <richardbr.personal@gmail.com>
2024-08-10 13:42:39 -0400

    Supabase TS

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,8 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
+import { Database } from "./supabase";
 
 const URL = "https://lqpdqexkwglhsmjzcbht.supabase.co";
 
 const API_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxcGRxZXhrd2dsaHNtanpjYmh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMzMDU1NDksImV4cCI6MjAzODg4MTU0OX0.BSS7nht1EZK1eBNFk99dxXg3bm6jNQ2_YNUjjudnq1E";
 
-export const supabase = createClient(URL, API_KEY);
+export const supabase = createClient<Database>(URL, API_KEY);
---
+++ src/utils/types.ts
@@ -0,0 +1,8 @@
+export type Creator = {
+  created_at: string;
+  description: string;
+  id: number;
+  imageURL: string;
+  name: string;
+  url: string;
+};
